0230027c5aba72e7817b4a755586a96e94367843
Pieter Beernink <60547491+pieterbnl@users.noreply.github.com>
2022-01-17 00:11:41 +0100

    Add project files.

---
+++ Qwixx/Program.cs
@@ -0,0 +1,22 @@
+﻿initiate();
+
+void initiate()
+{
+    header();
+    mainmenu();
+}
+
+void header()
+{
+    Console.WriteLine("*******");
+    Console.WriteLine("*QWIXX*");
+    Console.WriteLine("*******");
+    Console.WriteLine();
+}
+
+void mainmenu()
+{
+    Console.WriteLine("1. New game");
+    Console.WriteLine("2. High scores");
+    Console.WriteLine("3. Quite");
+}
