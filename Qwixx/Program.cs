ac059a7a34d181ad2f2f0682e71d6f61fce651ef
Pieter Beernink <60547491+pieterbnl@users.noreply.github.com>
2022-01-23 22:56:19 +0100

    change dice face concatenation display solution for colored dices

--- Qwixx/Program.cs
+++ Qwixx/Program.cs
@@ -16,144 +16,210 @@ namespace Qwixx
             //Interfacing.MainMenu();
 
             // Create faces for all dice eyes
-                        
-            DICE_ART.Add(1,               
-            new StringBuilder(
-            "┌─────────┐\n" +
-            "│         │\n" +
-            "│    0    │\n" +
-            "│         │\n" +
-            "└─────────┘"));
+            StringBuilder[] DICE1 = new StringBuilder[6];
+            DICE1[0] = new StringBuilder("┌─────────┐");
+            DICE1[1] = new StringBuilder("│         │");
+            DICE1[2] = new StringBuilder("│    0    │");
+            DICE1[3] = new StringBuilder("│         │");
+            DICE1[4] = new StringBuilder("└─────────┘");
+            DICE1[5] = new StringBuilder("white");
 
-            DICE_ART.Add(2,
-            new StringBuilder(
-            "┌─────────┐\n" +
-            "│  0      │\n" +
-            "│         │\n" +
-            "│      0  │\n" +
-            "└─────────┘"));
+            StringBuilder[] DICE2 = new StringBuilder[6];
+            DICE2[0] = new StringBuilder("┌─────────┐");
+            DICE2[1] = new StringBuilder("│  0      │");
+            DICE2[2] = new StringBuilder("│         │");
+            DICE2[3] = new StringBuilder("│      0  │");
+            DICE2[4] = new StringBuilder("└─────────┘");
+            DICE2[5] = new StringBuilder("yellow");
 
-            DICE_ART.Add(3,
-            new StringBuilder(
-            "┌────X────┐\n" +
-            "│  0      │\n" +
-            "│    0    │\n" +
-            "│      0  │\n" +
-            "└─────────┘"));
+            StringBuilder[] DICE3 = new StringBuilder[6];
+            DICE3[0] = new StringBuilder("┌─────────┐");
+            DICE3[1] = new StringBuilder("│  0      │");
+            DICE3[2] = new StringBuilder("│    0    │");
+            DICE3[3] = new StringBuilder("│      0  │");
+            DICE3[4] = new StringBuilder("└─────────┘");
+            DICE3[5] = new StringBuilder("blue");
 
-            DICE_ART.Add(4,
-            new StringBuilder(
-            "┌─────────┐\n" +
-            "│  0   0  │\n" +
-            "│         │\n" +
-            "│  0   0  │\n" +
-            "└─────────┘"));
+            StringBuilder[] DICE4 = new StringBuilder[6];
+            DICE4[0] = new StringBuilder("┌─────────┐");
+            DICE4[1] = new StringBuilder("│  0   0  │");
+            DICE4[2] = new StringBuilder("│         │");
+            DICE4[3] = new StringBuilder("│  0   0  │");
+            DICE4[4] = new StringBuilder("└─────────┘");
 
-            DICE_ART.Add(5,
-            new StringBuilder(
-            "┌─────────┐\n" +
-            "│  0   0  │\n" +
-            "│    0    │\n" +
-            "│  0   0  │\n" +
-            "└─────────┘"));
+            StringBuilder[] DICE5 = new StringBuilder[6];
+            DICE5[0] = new StringBuilder("┌─────────┐");
+            DICE5[1] = new StringBuilder("│  0   0  │");
+            DICE5[2] = new StringBuilder("│    0    │");
+            DICE5[3] = new StringBuilder("│  0   0  │");
+            DICE5[4] = new StringBuilder("└─────────┘");
+            DICE5[5] = new StringBuilder("white");
 
-            DICE_ART.Add(6,
-            new StringBuilder(
-            "┌─────────┐\n" +
-            "│  0   0  │\n" +
-            "│  0   0  │\n" +
-            "│  0   0  │\n" +
-            "└─────────┘"));           
+            StringBuilder[] DICE6 = new StringBuilder[6];
+            DICE6[0] = new StringBuilder("┌─────────┐");
+            DICE6[1] = new StringBuilder("│  0   0  │");
+            DICE6[2] = new StringBuilder("│  0   0  │");
+            DICE6[3] = new StringBuilder("│  0   0  │");
+            DICE6[4] = new StringBuilder("└─────────┘");
+            DICE6[5] = new StringBuilder("red");
 
-            // Add dices eyes that need to be displayed to array
-            List<int> dices = new List<int>();
-            dices.Add(1);
-            dices.Add(6);
-            dices.Add(3);
-            dices.Add(1);
-            dices.Add(2);
-            dices.Add(3);
-
+            // Add all dices that need to be displayed to an array
+            List<StringBuilder[]> DicesThrown = new List<StringBuilder[]>();
+            DicesThrown.Add(DICE2);
+            DicesThrown.Add(DICE6);
+            DicesThrown.Add(DICE3);
+                   
             // Display faces of all dices eyes
-            DisplayDices(dices);
+            DisplayDices(DicesThrown);
         }
 
         // Display faces of all passed dices eyes
-        internal static void DisplayDices(List<int> dices)
+        internal static void DisplayDices(List<StringBuilder[]> DicesThrown)
         {
-            string[] DiceFacesOutput = new string[5];            
-            int line = 0;
-
-            // Loop through all eyes (key values: 1 & 3)
-            foreach (int dice in dices)
+            // Loop trough all (5) lines that make up a dice face
+            for (int line = 0; line <= 4; line++)
             {
-                /*Console.WriteLine("Processing dice: " + dice + " ->> ");                
-                Console.WriteLine(DICE_ART[dice]); // prints complete dice face
-                Console.WriteLine();*/
-
-                // Split the dice face into lines, into an array                
-                string[] DiceFaceLines = DICE_ART[dice].ToString().Split("\n");
-                foreach (string s in DiceFaceLines)
+                // Loop through each thrown dice
+                foreach (StringBuilder[] dice in DicesThrown)
                 {
-                    DiceFacesOutput[line] = DiceFacesOutput[line] + s + "   ";
-                    /*Console.WriteLine(DiceFacesOutput[line]);*/
-                    line += 1;
-                }
-                line = 0;
-            }
+                    // Check the dice color and set the console display accordingly
+                    switch (dice[5].ToString())
+                    {
+                        case "yellow":
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            break;
+                        case "red":
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            break;
+                        case "blue":
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            break;
+                    }
 
-            // Loop through DiceFacesOutput
-            foreach (string s in DiceFacesOutput)
-            {                                
-                Console.WriteLine(s);
+                    // Write line from dice, including some white space
+                    Console.Write(dice[line]);
+                    Console.Write("    ");
+
+                }
+                // Break line, as now all thrown dices are looped
+                // and a full dice face line has been completed
+                Console.WriteLine();
             }
         }
+          
     }
 }
 
 
 
-
-/*foreach (KeyValuePair<int, StringBuilder> dice in DICE_ART)
-{
-    //Console.WriteLine("{0}, {1}", dice.Key, dice.Value);
-    Console.WriteLine(dice.Value);
-}*/
-
 /*
-            // Add all dice faces to an array
-            StringBuilder[] DiceFaces = { DiceFace1, DiceFace2, DiceFace3, DiceFace4, DiceFace5, DiceFace6 };
-            string[] DiceFacesOutput;
+DICE_ART.Add(1,
+new StringBuilder(
+"┌─────────┐\n" +
+"│         │\n" +
+"│    0    │\n" +
+"│         │\n" +
+"└─────────┘"));
 
-            // Combine faces from all rolled dices faces
+DICE_ART.Add(2,
+new StringBuilder(
+"┌─────────┐\n" +
+"│  0      │\n" +
+"│         │\n" +
+"│      0  │\n" +
+"└─────────┘"));
 
-            string[] DiceFaceLines = DiceFace3.ToString().Split("\\n");
-            Console.WriteLine("Content = ");
-            int i = 0;
-            foreach (string s in DiceFaceLines)
+DICE_ART.Add(3,
+new StringBuilder(
+"┌────X────┐\n" +
+"│  0      │\n" +
+"│    0    │\n" +
+"│      0  │\n" +
+"└─────────┘"));
+
+DICE_ART.Add(4,
+new StringBuilder(
+"┌─────────┐\n" +
+"│  0   0  │\n" +
+"│         │\n" +
+"│  0   0  │\n" +
+"└─────────┘"));
+
+DICE_ART.Add(5,
+new StringBuilder(
+"┌─────────┐\n" +
+"│  0   0  │\n" +
+"│    0    │\n" +
+"│  0   0  │\n" +
+"└─────────┘"));
+
+DICE_ART.Add(6,
+new StringBuilder(
+"┌─────────┐\n" +
+"│  0   0  │\n" +
+"│  0   0  │\n" +
+"│  0   0  │\n" +
+"└─────────┘"));*/
+
+
+/*internal static void DisplayDices(List<int> dices)
+{
+    string[] DiceFacesOutput = new string[5];
+    int line = 0;
+
+    // Loop through all eyes (key values: 1 & 3)
+    foreach (int dice in dices)
+    {
+        // Split the dice face into lines, into an array                
+        string[] DiceFaceLines = DICE_ART[dice].ToString().Split("\n");
+        foreach (string s in DiceFaceLines)
+        {
+            //DiceFacesOutput[line] = DiceFacesOutput[line] + s + "   ";
+
+            switch (dice)
             {
-                // i+=1;
-                // Console.WriteLine("i: " + i);
-                Console.WriteLine(s);
+                case 2:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case 6:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
             }
+            Console.Write(s);
+            //Console.WriteLine();
+            line += 1;
+        }
+        line = 0;
+    }
+
+    // Loop through DiceFacesOutput
+    foreach (string s in DiceFacesOutput)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(s);
+        Console.ForegroundColor = ConsoleColor.Black;
+    }*/
 
 
-            foreach (string s in lines)
-            {
-                Console.WriteLine(s);
-            }
 
+/*// Loop through all eyes (key values: 1 & 3)
+foreach (int dice in dices)
+{
+    // Split the dice face into lines, into an array                
+    string[] DiceFaceLines = DICE_ART[dice].ToString().Split("\n");
+    foreach (string s in DiceFaceLines)
+    {
+        DiceFacesOutput[line] = DiceFacesOutput[line] + s + "   ";
+        Console.WriteLine(DiceFacesOutput[line]);
+        line += 1;
+    }
+    line = 0;
+}
 
-
-            *//*  Console.Write(Dice1);
-              Console.WriteLine();
-              Console.Write(Dice2);
-              Console.WriteLine();
-              Console.WriteLine(Dice3);*//*
-            Console.WriteLine();
-
-            *//*StringBuilder Dices = new StringBuilder();          
-            Dices.Append(Dice1);
-            Dices.Append(Dice2);
-            Dices.Append(Dice3);
-            Console.WriteLine(Dices);*/
+// Loop through DiceFacesOutput
+foreach (string s in DiceFacesOutput)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine(s);
+    Console.ForegroundColor = ConsoleColor.Black;
+}*/
